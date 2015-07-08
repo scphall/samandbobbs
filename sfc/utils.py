aca1d243d5f5249a9947ed6ecda71e062efcbf62
Robyn Lucas <rlucas@cern.ch>
2015-07-08 11:15:49 +0100

    fix get_data in utils.py

--- sfc/utils.py
+++ sfc/utils.py
@@ -28,14 +28,14 @@ def get_data(filename, drop_data=False):
     if not os.path.exists(filename):
         raise IOError('No filename: {}'.format(filename))
     if filename.endswith('.zip'):
-        z = zipfile.ZipFile('../input/train.csv.zip')
+        z = zipfile.ZipFile(filename)
         filename = z.open(filename.replace('.zip', ''))
     data = pandas.read_csv(
         filename,
         parse_dates=['Dates'], infer_datetime_format=True,
         comment='#',
     )
-    data.Time = data.Time = data.Dates.map(lambda x: x.time())
+    data.Time = data.Dates.map(lambda x: x.time())
     if drop_data:
         data = data[(data.X < -121) & (data.Y < 40)]
         data = data.dropna()
